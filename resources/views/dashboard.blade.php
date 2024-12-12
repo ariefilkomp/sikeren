b2202ca63c7a76e150adb3a8c86454349e12231b
Arief Adi Nugroho <arief.ilkomp@gmail.com>
2024-12-12 11:31:57 +0700

    Membuat dark mode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-white">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-slate-600 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="max-w-3xl mx-auto text-center py-8">
                     <h2 class="text-4xl font-extrabold leading-tight tracking-tight text-slate-800 dark:text-white">
                         Aktivitas {{ $labelHari }}
@@ -18,7 +18,7 @@
 
                     <div class="mt-2">
                         <a href="{{ route('aktivitas.perBulan') }}" title=""
-                            class="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500">
+                            class="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-300">
                             Lihat Tanggal Lain
                             <svg aria-hidden="true" class="w-5 h-5 ml-2" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20" fill="currentColor">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white dark:bg-slate-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-900 dark:text-white">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
